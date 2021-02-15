2b37a080e7af658f9ea35657cbfe1a5641493624
s.filatov <nostro71@yandex.ru>
2021-02-15 17:29:13 +0300

    added localStorage

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,4 +1,14 @@
 import { createStore } from 'redux';
+import { throttle } from 'lodash';
 import { rootReducer } from './reducers';
+import { loadState, saveState } from './localStorage';
 
-export const store = createStore(rootReducer);
+const persistedState = loadState();
+
+export const store = createStore(rootReducer, persistedState);
+
+store.subscribe(throttle(() => {
+  saveState({
+    settings: store.getState().settings
+  })
+}, 1000));
